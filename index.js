741e4b983fcdfc6907b8bdab8550b4d7d2690852
Nina Mutty <ninamutty@gmail.com>
2016-11-18 15:03:10 -0800

    COUNT DOWN TO TURKEY DAY

--- index.js
+++ index.js
@@ -1,19 +1,52 @@
 // Your code here
 $(document).ready(function() {
 
-  var thanksGiving = new Date(2016, 11[, 24[, 0[, 0[, 0]]]])
-  console.log(thanksGiving);
+  var thanksGiving = new Date('November 24, 2016 00:00:00');
 
 
   var currentDate = function() {
     var today = new Date(Date.now());
     var options = { year: 'numeric', month: 'long', day: 'numeric', hour: 'numeric', minute: 'numeric', second: 'numeric' };
     var formatted = new Intl.DateTimeFormat('en-US', options).format(today);
-
-    $('#clock').html(formatted);
+    return new Date(formatted);
   };
 
-  var intervalID = setInterval(currentDate, 1000);
+  // var intervalID = setInterval(currentDate, 1000);
+
+
+  var timeTillThanks = function() {
+      var t = Date.parse(thanksGiving) - Date.parse(currentDate());
+      var seconds = Math.floor( (t/1000) % 60 );
+      var minutes = Math.floor( (t/1000/60) % 60 );
+      var hours = Math.floor( (t/(1000*60*60)) % 24 );
+      var days = Math.floor( t/(1000*60*60*24) );
+      return {
+        'total': t,
+        'days': days,
+        'hours': hours,
+        'minutes': minutes,
+        'seconds': seconds
+      };
+  }
+
+
+  var initializeClock = function(id, thankstime) {
+    var clock = document.getElementById(id);
+    console.log(clock);
+
+    var t = timeTillThanks(thankstime);
+    var clockFormat = 'days: ' + t.days + '<br>' +
+                      'hours: '+ t.hours + '<br>' +
+                      'minutes: ' + t.minutes + '<br>' +
+                      'seconds: ' + t.seconds;
+    $('#clock').html(clockFormat);
+  }
+
+  // initializeClock('clock', thanksGiving);
+  // var intervalID = setInterval(timeTillThanks, 1000);
+
+
+  var timeinterval = setInterval(initializeClock, 1000);
 
 
 
@@ -25,18 +58,11 @@ $(document).ready(function() {
 
 
 
-
-
-
-
-
-
-
-
-  $('#clock').css('font-size', '3rem');
-  $('#clock').css('color', 'black');
-  $('#clock > h2').css('margin-top', '0%');
-  $('#clock').css('padding', '0%');
-  $('#clock').css('width', '70%');
+  // 
+  // $('#clock').css('font-size', '3rem');
+  // $('#clock').css('color', 'black');
+  // $('#clock > h2').css('margin-top', '0%');
+  // $('#clock').css('padding', '0%');
+  // $('#clock').css('width', '70%');
 
 });
