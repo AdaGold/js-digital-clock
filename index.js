f8c655e7534a243c5de51fc602df00e9179f933e
Erin Bond <erin@erinebond.com>
2016-11-18 13:14:41 -0800

    Displays current time

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-// Your code here
+//index.js
+
+$(document).ready(function (){
+
+   var now = new Date(Date.now());
+  var showTime = function(){
+     setInterval(
+
+
+     $('#clock').append('<h1>' + now.getHours() + ":" + now.getMinutes() + ":" + now.getSeconds() + '</h1>'),
+     1000)
+   }
+
+   showTime();
+
+});
