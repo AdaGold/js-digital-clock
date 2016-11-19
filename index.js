47e92e5b62fae7bb8f6ae03d19c252687672fd63
jm-rives <joanna.rives@gmail.com>
2016-11-19 10:10:27 -0800

    INITIAL COMMIT

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
-// Your code here
+// Getting Started
+
+// [ ] Create a function which will do all of the work to calculate and show the time.
+
+// [ ] Use the Date Library in Javascript to retrieve the current date and time information. Use the different methods that are provided to you for retrieving the individual () hour, () minute and () second information.
+
+// [ ] Use the () setInterval method to utilize the function you've created to update each second.
+
+// [ ] Your solution will include one CSS file which contains your page styles, one JS file which contains your clock logic and one HTML file which contains your page structure and loads the CSS and JS resources.
+// [] use date library
+'use strict';
+
+var DigitalClock = function () {
+  
+
+  var currentTime = new Date ( );
+
+  
+} 
+
+
+$(document).ready(function()
+{
+  console.log("Our document is loaded!");
+   // setInterval('updateClock()', 1000);
+});
+
+
+console.log("At the bottom of the JS file");
